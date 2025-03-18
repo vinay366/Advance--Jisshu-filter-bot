551dcc82502d3e652ce4a05c7fd4977d021a12b3
Vinay Mudiraj <78520329+vinay366@users.noreply.github.com>
2025-03-18 15:11:37 +0530

    Update Redeem.py

--- plugins/Extra/Redeem.py
+++ plugins/Extra/Redeem.py
@@ -44,8 +44,8 @@ Aᴍᴏᴜɴᴛ:</b> {num_codes}
 
         keyboard = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="http://t.me/NehaTestBot")],
-                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/IM_JISSHU")]
+                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="http://t.me/PriyankaArulMohanV3Bot")],
+                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/VinayMudhiraj")]
             ]
         )
 
